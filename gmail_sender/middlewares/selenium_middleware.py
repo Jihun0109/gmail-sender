79463ccae1b0cd7ea5a1ce82298ea5cba3fad30c
Jihun0109 <beautisong@hotmail.com>
2018-09-30 17:26:47 +0800

    all functions implemented

--- gmail_sender/middlewares/selenium_middleware.py
+++ gmail_sender/middlewares/selenium_middleware.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 
@@ -62,11 +63,21 @@ class SeleniumMiddleware(object):
 
             if spider.name == "gmail_sender":
                 # Opend login window
-                compose_elem = self.login_google("zz@gmail.com","zz")
-                if compose_elem:
-                    print "Login Successed!!"
-                else:
+                compose_elem = self.login_google("creative1230109@gmail.com","fmdfkehd1231320109")
+                if compose_elem == None:
                     print "Login Failed."
+                else:
+                    print "Login Successed!!"
+
+                    if self.open_compose(compose_elem):
+                        self.write_receiver_addresses(["sdf@hotmail.com"])
+                        self.write_subject("Hello.")
+                        self.write_content("This is test email.")
+                        send_button_elem = self.d.find_element_by_xpath('//*[@aria-label="Send ‪(Ctrl-Enter)‬"]')
+                        send_button_elem.click()
+                        print "Send emails...."
+                        time.sleep(30)
+
 
             resp = TextResponse(url=self.d.current_url,
                                 body=self.d.page_source,
@@ -74,6 +85,38 @@ class SeleniumMiddleware(object):
             resp.request = request.copy()
             #self.d.quit()
             return resp
+
+    def write_receiver_addresses(self, email_list):
+        string_to_write = ",".join(email_list).strip() + ","
+        to_input = self.d.find_element_by_xpath('//form[@enctype]//textarea[@name="to"]')
+        to_input.click()
+        to_input.send_keys(string_to_write)
+        print "Wrote email addresses"
+        time.sleep(10)
+
+    def write_subject(self, subject):
+        to_input = self.d.find_element_by_xpath('//form[@enctype]//input[@name="subjectbox"]')
+        to_input.click()
+        to_input.send_keys(subject)
+        print "Wrote subject content."
+        time.sleep(10)
+
+    def write_content(self, content):
+        to_input = self.d.find_element_by_xpath('//div[@aria-label="Message Body"]')
+        #to_input.click()
+        #to_input.send_keys(content)
+        
+        query = 'arguments[0].appendChild(document.createElement("div").appendChild(document.createTextNode("Hi")));'        
+        self.d.execute_script(query,to_input)
+        print "Wrote content."
+        time.sleep(10)
+
+    def open_compose(self, compose_elem):
+        compose_elem.click()
+        input_mail_to = self.wait_for_load_element('//form[@enctype]//textarea[@name="to"]', 30)
+        if input_mail_to == None:
+            return False        
+        return True
 
     def login_google(self, email, password):
         email_edit  = self.d.find_element_by_xpath('//*[@type="email"]')
@@ -97,6 +140,15 @@ class SeleniumMiddleware(object):
                 return None
             else:
                 return compose_elem
+
+    def wait_for_load_element(self, xpath, timeout=30):
+        try:
+            elem = WebDriverWait(self.d, timeout).until(ec.visibility_of_element_located((By.XPATH, xpath)))
+        except TimeoutException:
+            print "Timed out waiting for element to load."
+            return None
+        else:
+            return elem
 
 
 ###########################################################
